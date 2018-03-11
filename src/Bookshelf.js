08b77377bdeb0900b6f8ede5cfe36f41bb50f95f
edward ingram <unquieted@gmail.com>
2018-03-11 10:33:20 -0400

    Continued refactoring & map over array of book objects to display in bookshelf

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,50 +1,45 @@
-import React, { Component } from 'react'
-import Book from './Book'
+import React, { Component } from "react";
+import Book from "./Book";
 
 class Bookshelf extends Component {
-
   render() {
-
     return (
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {this.props.books
+                .filter(book => book.shelf === "currentlyReading")
+                .map(book => <Book book={book} />)}
+            </ol>
+          </div>
+        </div>
 
-<div>
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
-              <Book books={this.props.books.filter(book => book.shelf === 'currentlyReading')}/>
-            </li>
-          </ol>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Want To Read</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {this.props.books
+                .filter(book => book.shelf === "wantToRead")
+                .map(book => <Book book={book} />)}
+            </ol>
+          </div>
+        </div>
+
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Already Read</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {this.props.books
+                .filter(book => book.shelf === "read")
+                .map(book => <Book book={book} />)}
+            </ol>
+          </div>
         </div>
       </div>
-
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Want to Read</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
-              <Book books={this.props.books.filter(book => book.shelf === 'wantToRead')}/>
-            </li>
-          </ol>
-        </div>
-      </div>
-
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Already Read</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
-              <Book books={this.props.books.filter(book => book.shelf === 'read')}/>
-            </li>
-          </ol>
-        </div>
-      </div>
-
-</div>
-
-    )
+    );
   }
 }
 
-export default Bookshelf
+export default Bookshelf;
